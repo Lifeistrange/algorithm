d72f4095a5737aa2e7da10938c2d8f0af0c4fda0
yuanbaoping <yuanbaoping@bbdservice.com>
2018-06-21 18:01:32 +0800

    add bfs

---
+++ src/main/java/com/lifeiscoding/algorithm/graph/path/BreadthFirstPaths.java
@@ -0,0 +1,52 @@
+package com.lifeiscoding.algorithm.graph.path;
+
+import com.lifeiscoding.algorithm.graph.Graph;
+
+import java.util.Queue;
+import java.util.Stack;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class BreadthFirstPaths implements IPaths {
+
+    private final int s;
+    private boolean[] marked;
+    private int[] edgeTo;
+
+    public BreadthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        bfs(G, s);
+    }
+
+    private void bfs(Graph G, int s) {
+        Queue<Integer> queue = new LinkedBlockingQueue<>();
+        marked[s] = true;
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    marked[w] = true;
+                    queue.offer(w);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    @Override
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<>();
+        for (int x = v; x != s; x = edgeTo[x]) {
+            path.push(x);
+        }
+        path.push(s);
+        return path;
+    }
+}
